c431b547706cd5e69c6cc4368a1deca77b0b393b
Lúcás Meier <cronokirby@gmail.com>
2018-10-25 15:33:47 +0200

    Refactors line filtering

--- src/Cloc.hs
+++ src/Cloc.hs
@@ -17,36 +17,65 @@ import TextPipes
 textShow :: Show s => s -> T.Text
 textShow = T.pack . show
 
+
+-- | Represents the line counts for some file
+data LineCount = LineCount
+    { codeLines :: Int
+    , commentLines :: Int
+    }
+
+instance Semigroup LineCount where
+    LineCount c1 cm1 <> LineCount c2 cm2 = LineCount (c1 + c2) (cm1 + cm2)
+
+instance Monoid LineCount where
+    mempty = LineCount 0 0
+    mappend = (<>)
+
+
 -- | Represents a counted file along with filepath and line count
 data CountElement = CountElement 
     { file :: FilePath
-    , codeLines :: Int
+    , count :: LineCount
     }
 
-
-totalCount :: [CountElement] -> Int
-totalCount = foldr go 0
+totalCount :: [CountElement] -> LineCount
+totalCount = foldr go mempty
   where
-    go (CountElement _ i) acc = i + acc
+    go (CountElement _ i) acc = i <> acc
 
 prettyCount :: CountElement -> T.Text
-prettyCount (CountElement fp i)  =
-    T.pack fp <> ": " <> textShow i <> " lines"
+prettyCount (CountElement fp l)  =
+    let code = textShow (codeLines l)
+        comment = textShow (commentLines l)
+    in T.pack fp 
+        <> ": " 
+        <> code <> " lines ("
+        <> comment <> " comments)"
 
 
-isComment :: T.Text -> Bool
-isComment = not . T.isPrefixOf "--"
+-- | Represents a predicate that is true if the line is a comment
+newtype LineFilter = LineFilter (T.Text -> Bool)
+
+
+isComment :: LineFilter
+isComment = LineFilter (T.isPrefixOf "--")
 
 countLines :: FilePath -> IO CountElement
 countLines fp = IO.withFile fp IO.ReadMode $ \h -> do
-    let lines = textLines h >-> P.filter isComment
-    lineCount <- P.length lines
+    let lines = textLines h
+    lineCount <- P.fold (go isComment) mempty id lines
     return (CountElement fp lineCount)
+  where
+    go :: LineFilter -> LineCount -> T.Text -> LineCount
+    go (LineFilter f) (LineCount code comment) line =
+        if f line
+            then LineCount code (comment + 1)
+            else LineCount (code + 1) comment
 
 run :: [FilePath] -> IO ()
 run files = do
     counts <- forM files countLines
-    let total = totalCount counts
+    let LineCount total _ = totalCount counts
         totalTxt = "\nTotal Lines: " <> textShow total
     forM_ counts (T.putStrLn . prettyCount)
     T.putStrLn totalTxt
